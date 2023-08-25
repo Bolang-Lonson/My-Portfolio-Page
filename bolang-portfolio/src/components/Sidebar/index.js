cf7a05ba1c9b34f565c30efb69bbd1b4cc76a501
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-08-25 12:40:41 +0100

    social links
    Social links added to navbar eg Linkedin, Github, Skype

--- bolang-portfolio/src/components/Sidebar/index.js
+++ bolang-portfolio/src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faSkype } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className='nav-bar'>
@@ -22,6 +23,23 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={ faEnvelope } color='#4d4d4e'/>
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/bolang-lonson-nfi-11772224a/'>
+          <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+        </a>
+      </li>
+      <li>
+        <a target='_blank' rel='noreferrer' href='https://github.com/Bolang-Lonson'>
+          <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
+        </a>
+      </li>
+      <li>
+        <a target='_blank' rel='noreferrer' href='skype:live:Bolang Nfi'>
+          <FontAwesomeIcon icon={faSkype} color='#4d4d4e'/>
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
