2cc3940515f62be1debf64c71b9524c7da4cec2c
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-08-27 22:02:35 +0100

    introducing animated letters

--- bolang-portfolio/src/components/AnimatedLetters/index.js
+++ bolang-portfolio/src/components/AnimatedLetters/index.js
@@ -2,15 +2,15 @@ import './index.scss'
 
 const AnimatedLetters = ({ letterClass, strArray, idx }) => {
     return (
-        <span>
-            {
-                strArray.map((char, i) => (
-                    <span key={char + i} className={`${letterClass} _${i + idx}`}>
-                        {char}
-                    </span>
-                ))
-            }
-        </span>
+      <span>
+        {
+          strArray.map((char, i) => (
+            <span key={char + i} className={`${letterClass} _${i + idx}`}>
+              {char}
+            </span>
+          ))
+        }
+      </span>
     )
 }
 
